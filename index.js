3d53e560d7b4e92b12493b47eade27c8cfac2d9b
emmanuel-nwafor <167652757+emmanuel-nwafor@users.noreply.github.com>
2024-05-19 17:29:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function bgChange2() {
+  let bg2 = document.querySelector('.mainContainer');
+  bg2.style.backgroundImage = 'url("bg3.jpg")';
+  if (bg2 = 'url("bg3.jpg")') {
+    bg2.innerHTML = 'url("bg3.jpg)';
+  }
+    else {
+      bg2.innerHTML = 'url("bg.jpg")'
+    }
+}
+function order() {
+  alert('your order has been placed, we will get back to you shortly')
+}
+
+
+function click() {
+  
+const navbar = document.querySelector('.container');
+
+navbar.style.display = 'flex'
+
+}
+
+/*const toggleMenu = () => {
+  const nav = document.getElementById('navbar');
+  if (nav.className === 'navbar') {
+    nav.className += ' responsive';
+  } 
+  
+  else {
+    nav.className = 'navbar';
+  }
+}*/
